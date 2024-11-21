5e8f7aa80115f1200a4159c518cf0340938c777e
Luis Villa <lvillap@gmail.com>
2024-11-21 23:36:56 +0100

    refactor: extraction of feature logic

--- main.ts
+++ main.ts
@@ -1,75 +1,11 @@
+import { registerTaskLatencyLabels } from "features/latencyTimes/latency-times";
 import { Plugin } from "obsidian";
-import { EditorView, Decoration, ViewPlugin, ViewUpdate, WidgetType, Range, DecorationSet } from "@codemirror/view";
-import { createLatencyTextFor, isTaskWithDate } from "features/latencyTimes/latency-times";
 
 export default class TaskLatencyPlugin extends Plugin {
 
 	onload() {
 
-		const taskDecorator = ViewPlugin.fromClass(
-			class {
-				decorations: any;
+		registerTaskLatencyLabels(this);
 
-				constructor(view: EditorView) {
-					this.decorations = this.decorateTasks(view);
-				}
-
-				update(update: ViewUpdate) {
-					if (update.docChanged || update.viewportChanged) {
-						this.decorations = this.decorateTasks(update.view);
-					}
-				}
-
-				decorateTasks(view: EditorView): DecorationSet {
-
-					const widgets: Range<Decoration>[] = [];
-
-					for (const { from, to } of view.visibleRanges) {
-
-						const text = view.state.doc.sliceString(from, to);
-						const lines = text.split("\n");
-
-						let offset = from;
-						lines.forEach((line) => {
-
-							if (
-								isTaskWithDate(line)
-							) {
-
-								const latencyText = createLatencyTextFor(line);
-
-								const deco = Decoration.widget({
-									widget: new class extends WidgetType {
-										toDOM() {
-											const span = document.createElement("span");
-											span.className = "latency-time";
-											span.textContent = ` ${latencyText}`;
-											return span;
-										}
-
-										updateDOM(prev: HTMLElement) {
-											prev.textContent = ` ${latencyText}`;
-											return true;
-										}
-									}(),
-									side: 1,
-								});
-
-								if (offset + line.length <= view.state.doc.length) {
-									widgets.push(deco.range(offset + line.length));
-								}
-							}
-							offset += line.length + 1;
-						});
-					}
-					return Decoration.set(widgets, true); // Forzar consistencia en las decoraciones
-				}
-			},
-			{
-				decorations: (v) => v.decorations,
-			}
-		);
-
-		this.registerEditorExtension(taskDecorator);
 	}
 }
